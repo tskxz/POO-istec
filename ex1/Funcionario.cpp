6def2715009c1c21d1038dc0c7368d3f5c258d87
Tanjil <tanjilkh@gmail.com>
2024-10-05 18:39:48 +0100

    Create Funcionario.cpp

---
+++ ex1/Funcionario.cpp
@@ -0,0 +1,24 @@
+#include "Funcionario.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+Funcionario::Funcionario(string nome, double salario){
+	this->nome = nome;
+	this->salario = salario;
+}
+
+void Funcionario::addAumento(double valor){
+	this->salario += valor;
+}
+
+double Funcionario::ganhoAnual(){
+	return this->salario * 12;
+}
+
+void Funcionario::exibeDados(){
+	cout << "Nome: " << this->nome << endl;
+	cout << "Salario: " << this->salario << endl;
+	cout << "Ganho Anual: " << ganhoAnual() << endl;
+}
