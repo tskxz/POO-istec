62eca6454481496d8e811e7f87a060a6a3d624ee
Tanjil <tanjilkh@gmail.com>
2024-10-05 20:13:34 +0100

    Update Funcionario.h

--- ex1/Funcionario.h
+++ ex1/Funcionario.h
@@ -1,18 +1,21 @@
 #ifndef FUNCIONARIO_H 
-#define Funcionario_H
+#define FUNCIONARIO_H
 #include <string>
 
 using namespace std;
 
 class Funcionario{
+	protected:
 	string nome;
 	double salario;
 	
 	public:
 		Funcionario(string nome, double salario);
 		void addAumento(double valor);
-		double ganhoAnual();
-		void exibeDados();
+		double getSalario();
+		void setSalario();
+		virtual double ganhoAnual();
+		virtual void exibeDados();
 };
 
 #endif
