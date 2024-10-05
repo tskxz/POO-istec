51d152f03f87f0cab125aa06300b936e029022c9
Tanjil <tanjilkh@gmail.com>
2024-10-05 20:13:30 +0100

    Update main.cpp

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include "Funcionario.h"
+#include "Assistente.h"
+#include "Tecnico.h"
 #include <string>
 using namespace std;
 
 int main(){
 	Funcionario func1("tanjil", 800);
 	func1.exibeDados();
+
+	Assistente ass1("ricardo", 800, 202291);
+	ass1.exibeDados();
+
+	Tecnico tec1("Luis", 900, 2022199, 100);
+	tec1.exibeDados();
+
 }
+
