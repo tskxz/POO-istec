a428e62b86f02099d917249044c113164a793aa2
Tanjil <tanjilkh@gmail.com>
2024-10-07 12:01:43 +0100

    Create Miseravel.cpp

---
+++ c++/ex2/Pessoa/Miseravel.cpp
@@ -0,0 +1,10 @@
+#include "Miseravel.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Miseravel::Miseravel() : Pessoa(){};
+void Miseravel::mendiga(){
+	cout << "a mendigar..." << endl;
+};
