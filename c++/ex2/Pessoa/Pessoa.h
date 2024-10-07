8ed8a1f833f3dc25bb6704fc58ae58128a7a9891
Tanjil <tanjilkh@gmail.com>
2024-10-07 12:01:34 +0100

    Create Pessoa.h

---
+++ c++/ex2/Pessoa/Pessoa.h
@@ -0,0 +1,17 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Pessoa {
+	protected:
+		string nome;
+		int idade;
+	public:
+		Pessoa();
+};
+
+#endif
