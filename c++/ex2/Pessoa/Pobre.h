c0696032355d71d275e3c0268703caae5fd0fde9
Tanjil <tanjilkh@gmail.com>
2024-10-07 12:01:31 +0100

    Create Pobre.h

---
+++ c++/ex2/Pessoa/Pobre.h
@@ -0,0 +1,13 @@
+#ifndef POBRE_H
+#define POBRE_H
+
+#include <iostream>
+#include <string>
+#include "Pessoa.h"
+
+class Pobre : public Pessoa {
+	public:
+		Pobre();
+		void trabalha();
+};
+#endif
